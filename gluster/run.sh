efd983f57e6639813b3e5abdc031f380406ef1df
Alessandro Chitolina <alekitto@gmail.com>
2020-01-09 23:42:36 +0100

    mount bind host dev dir

--- gluster/run.sh
+++ gluster/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+HOST_DEV_DIR=${HOST_DEV_DIR:-/mnt/host-dev}
 LOG_LEVEL=${LOG_LEVEL:-INFO}
+
+if [ -c "${HOST_DEV_DIR}/zero" ] && [ -c "${HOST_DEV_DIR}/null" ]; then
+    # looks like an alternate "host dev" has been provided
+    # to the container. Use that as our /dev ongoing
+    mount --rbind "${HOST_DEV_DIR}" /dev
+fi
 
 /sbin/glustereventsd &
 /sbin/glusterd -N --log-file=- --log-level=$LOG_LEVEL $GLUSTERD_OPTIONS &
